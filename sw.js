fe73d48ca475ef33937cdf2cfb2efb04ce91ee03
rmsfid11-commits <rmsfid11-commits@users.noreply.github.com>
2026-02-13 23:46:34 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'myfinance-v1';
+const urlsToCache = ['./', './index.html', './manifest.json', './favicon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(names => Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.status === 200 && res.type === 'basic') {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
